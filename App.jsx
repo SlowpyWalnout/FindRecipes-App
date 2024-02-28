cc9d6f76bee24499ed31dc1a82d56c92e764a6a7
Customi24 <Customi24@gmail.com>
2024-02-28 14:19:40 -0600

    api

--- App.jsx
+++ App.jsx
@@ -3,6 +3,24 @@ import { StyleSheet, Text, View } from 'react-native';
 import React from 'react';
 
 export default function App() {
+
+  
+  const [recipe, setRecipe] = useState('');
+  const [recipeData, setRecipeData] = useState(null);
+  const [error, setError] = useState(null);
+
+  const fetchRecipeApp = async () => {
+    try {
+      const res = await fetch('https://api.edamam.com/doc/open-api/recipe-search-v2.json');
+      const data = await res.json();
+      setRecipeData(data);
+      setError(null);
+    } catch (error) {
+      console.error(error);
+      setError('Error fetching Recipe data');
+    }
+  };
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
