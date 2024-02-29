faa3e845315ee86ab83f38a541b9e0b4d0bb4dce
José Quintana <76418657+SlowpyWalnout@users.noreply.github.com>
2024-02-29 08:22:00 -0600

    component of recipecards is ready

--- App.jsx
+++ App.jsx
@@ -1,14 +1,28 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ScrollView} from 'react-native';
 import React from 'react';
+import RecipeSheet from './src/components/recipesheet';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style = {styles.container}>
+      <ScrollView>
+        <RecipeSheet
+          name="Pizza"
+          image="https://www.laespanolaaceites.com/wp-content/uploads/2019/06/pizza-con-chorizo-jamon-y-queso-1080x671.jpg"
+          description="This is a pizza"
+          ingredients="Flour, Cheese, Tomato, Olive, Mushroom"
+        />
+        <RecipeSheet
+          name="Burger"
+          image={"https://sevilla.abc.es/contenidopromocionado/wp-content/uploads/sites/2/2019/09/portada-wp-burguer.jpeg"}
+          description="This is a burger"
+          ingredients="Bread, Cheese, Tomato, Onion, Meat"
+        />
+      </ScrollView>
     </View>
-  );
+    );
+    
 }
 
 const styles = StyleSheet.create({
@@ -17,5 +31,7 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    marginTop: 20,
+    height: "100%",
   },
 });
